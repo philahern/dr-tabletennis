bb343a6ba98f51b5c5ea2a93e53fb2421670dc39
Phil Ahern <pahern@digitalriver.com>
2017-03-29 22:10:30 +0100

    Added match notifications (cloud functions)

---
+++ src/helpers/notifications.js
@@ -0,0 +1,35 @@
+import { db, messaging } from '../config/constants'
+
+export function notifyRequestPermission () {
+  return messaging.requestPermission()
+                  .then(function() {
+                    notifyGetToken()
+                  })
+}
+
+export function notifyOnTokenRefresh () {
+  messaging.onTokenRefresh(function() {
+    notifyGetToken()
+  });
+}
+
+export function notifyGetToken () {
+  return messaging.getToken()
+          .then(function(token) {
+            console.log('Token received.', token)
+            if (token !== null) {
+              sendTokenToServer(token)
+            }
+          })
+          .catch(function(err) {
+            console.log('Unable to retrieve token ', err)
+          });
+}
+
+function sendTokenToServer(token) {
+  const player = window.localStorage.getItem('loggedInAs')
+  console.log('Token sent to server.');
+  return db.ref('/users/' + player + '/notificationTokens/' + token).set(true);
+   
+}
+
